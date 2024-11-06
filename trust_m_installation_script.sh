fee928a66934749e2f91305b2d34b11a797ab45c
ying-css <71699179+ying-css@users.noreply.github.com>
2024-11-06 09:51:43 +0800

    Updated submodule with trustm_lib v5.x  (#11)
    1)updated submodule with trustm_lib v5.x

--- trust_m_installation_script.sh
+++ trust_m_installation_script.sh
@@ -1,27 +1,16 @@
 #!/bin/sh
 
-FILE="0001-trust_m_lib.patch"
 LINUX_TOOLS_PATH="Python_TrustM_GUI/linux-optiga-trust-m/"
 TRUSTM_LIB_PATH="${LINUX_TOOLS_PATH}trustm_lib/"
 CURRENT_DIR="${PWD}"
-PATCH="${PWD}/${FILE}"
 
 
 sudo apt update 
-sudo apt -y install awscli git gcc libssl-dev python-wxtools 
-sudo apt install python3-pubsub xxd
+sudo apt -y install awscli git gcc libssl-dev gpiod libgpiod-dev
+sudo apt install python3-pubsub xxd wxpython-tools
 
-
-echo $PATCH
-echo $TRUSTM_LIB_PATH
 echo $LINUX_TOOLS_PATH
-
-cp $PATCH $LINUX_TOOLS_PATH
 cd $LINUX_TOOLS_PATH
-echo "-----> Entering ${PWD}"
-git reset --hard
-git apply $FILE
-#rm $FILE
 set -e
 echo "-----> Build Trust M Linux Tools"
 sudo make uninstall
